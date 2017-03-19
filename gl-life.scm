85f1a6757ce327492030167ea1d7d5126d88c86e
Vasilij Schneidermann <v.schneidermann@gmail.com>
2017-03-19 22:13:07 +0100

    Implement part 2

--- gl-life.scm
+++ gl-life.scm
@@ -6,13 +6,46 @@
 (define width 500)
 (define height 500)
 
-(define triangle
-  (glu:make-mesh
-   vertices: '(attributes: ((position float: 3))
-               initial-elements: ((position
-                                    0    0.5 0
-                                   -0.5 -0.5 0
-                                    0.5 -0.5 0)))))
+(define rows 10)
+(define columns 10)
+
+(define-record cell x y vao)
+
+(define (cell-vertices x y)
+  (define (->normalize n) (- (* n 2) 1))
+  (let ((x1 (->normalize (/ x columns)))
+        (x2 (->normalize (/ (+ x 1) columns)))
+        (y1 (->normalize (/ y rows)))
+        (y2 (->normalize (/ (+ y 1) rows))))
+    (list x1 y1 0
+          x1 y2 0
+          x2 y2 0
+
+          x1 y1 0
+          x2 y1 0
+          x2 y2 0)))
+
+(define (create-cell prog x y)
+  (let ((mesh
+         (glu:make-mesh
+          vertices:
+          `(attributes: ((position float: 3))
+            initial-elements: ((position . ,(cell-vertices x y)))))))
+    (glu:mesh-make-vao! mesh `((position . ,(gl:get-attrib-location
+                                             prog "position"))))
+    (make-cell x y (glu:mesh-vao mesh))))
+
+(define (create-board prog)
+  (let ((board (make-vector (* rows columns))))
+    (do ((row 0 (add1 row)))
+        ((= row rows))
+      (do ((column 0 (add1 column)))
+          ((= column columns))
+        (vector-set! board (+ (* row columns) column)
+                     (create-cell prog column row))))
+    board))
+
+(define square-vertices 6) ; ugh
 
 (define vertex-shader-source
   "#version 120
@@ -35,12 +68,17 @@
                                           fragment-shader-source)))
     (glu:make-program (list vertex-shader fragment-shader))))
 
-(define (draw! vao window prog)
+(define (draw! board window prog)
   (gl:clear (bitwise-ior gl:+color-buffer-bit+ gl:+depth-buffer-bit+))
   (gl:use-program prog)
 
-  (gl:bind-vertex-array vao)
-  (gl:draw-arrays gl:+triangles+ 0 (glu:mesh-n-vertices triangle))
+  (do ((row 0 (add1 row)))
+      ((= row rows))
+    (do ((column 0 (add1 column)))
+        ((= column columns))
+      (let ((vao (cell-vao (vector-ref board (+ (* row columns) column)))))
+        (gl:bind-vertex-array vao)
+        (gl:draw-arrays gl:+triangles+ 0 square-vertices))))
 
   (glfw:poll-events)
   (glfw:swap-buffers window))
@@ -52,13 +90,11 @@
                      context-version-minor: 1)
     (let ((window (glfw:window)))
       (glfw:make-context-current window)
-      (let ((prog (init-gl!)))
-        (glu:mesh-make-vao! triangle `((position . ,(gl:get-attrib-location
-                                                     prog "position"))))
-        (let ((vao (glu:mesh-vao triangle)))
-          (let loop ()
-            (when (not (glfw:window-should-close window))
-              (draw! vao window prog)
-              (loop))))))))
+      (let* ((prog (init-gl!))
+             (board (create-board prog)))
+        (let loop ()
+          (when (not (glfw:window-should-close window))
+            (draw! board window prog)
+            (loop)))))))
 
 (main)
